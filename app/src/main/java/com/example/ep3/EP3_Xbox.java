57d50ecb81caa7dc63f6bbb1a9d4d7a794b2cd89
enyel01 <angelus_gabriel15@hotmail.com>
2020-11-07 13:07:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ep3/EP3_Xbox.java
@@ -0,0 +1,14 @@
+package com.example.ep3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EP3_Xbox extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_e_p3__xbox);
+    }
+}
